dd7905aad693531168be9f918aeddf57a9f4b350
kuee <mblabuh@dmacc.edu>
2025-02-04 19:13:41 -0800

    Add new PHP file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<style>
+body{
+background-color: blue;
+}
+h1{
+	color:red;
+	}
+	
+h2{
+	color:magenta;
+	}
+	
+p{
+	font-family:20px;
+    color:black;	
+	}
+</style>
+
+
+</head>
+<body>
+
+<?php
+echo "wdv341.php";
+echo"<h1> My Homework Page </h1>";
+echo"<h2> Course WDV341 Fall 2025</h2>";
+echo"<p> This page will feature all my projects and some things I plan on creating throughout this class.</p>";
+echo"<p>Go Hawkeyes!</p>"; 
+echo '<a href="https://dmacc.instructure.com/courses/18722/assignments/688723">Check it out</a>';
+?>
+
+
+
+</body>
+</html>
+
