80cbf0eee6a516168ec595a46e0e370db4a4b622
kuee <mblabuh@dmacc.edu>
2025-02-06 18:26:07 -0800

    Add new PHP basic

---
+++ basicph.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<?php
+
+//1. Create variable and print
+$yourName = "Messan, Blabuh";
+
+//2. dispaly assignment name in h1 element
+echo "<h1>PHP Basics!</h1>";
+
+//3. Display HTML directly
+  echo "<h2>$yourName</h2>";
+
+// 4. Define numbers and perform a calculation
+   $number1 = 1;
+   $number2 = 2;
+   $total = $number1 + $number2; 
+   
+// 5. Disppay value of variables
+echo "<p>Number 1: $number1</p>";
+echo "<p>Number 2: $number2</p>";
+echo "<p>Total: $total</p>";
+
+//  6.Create php array
+
+$app = array("PHP", "HTML", "JavaScript");
+
+// convert from php to JavaScript
+
+echo "<script type='text/javascript'>";
+
+// built javascript array
+
+echo "var javarray = [";
+
+// go through php array to generate javarray array
+   foreach ($cars as $course) {
+    echo "'$course', "; // Add each value to the JavaScript array
+}
+
+echo "];";
+
+// Log the array in the console
+echo "console.log(javarray);";
+
+echo "</script>";
+
+
+?>
+
+
+
+
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -2,25 +2,7 @@
 
 <html>
 <head>
-<style>
-body{
-background-color: blue;
-}
-h1{
-	color:red;
-	}
-	
-h2{
-	color:magenta;
-	}
-	
-p{
-	font-family:20px;
-    color:black;	
-	}
-</style>
-
-
+<link rel="stylesheet" type="text/css" href="stylesheet.css" />	
 </head>
 <body>
 
@@ -30,7 +12,8 @@ echo"<h1> My Homework Page </h1>";
 echo"<h2> Course WDV341 Fall 2025</h2>";
 echo"<p> This page will feature all my projects and some things I plan on creating throughout this class.</p>";
 echo"<p>Go Hawkeyes!</p>"; 
-echo '<a href="https://dmacc.instructure.com/courses/18722/assignments/688723">Check it out</a>';
+echo '<p><a href="https://dmacc.instructure.com/courses/18722/assignments/688723">Check it out<p></a>';
+echo '<a href="basic.php">Go to Basic Page</a>';
 ?>
 
 
